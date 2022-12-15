61bf8d5ad16c10b2e59a5510c3087581d166b4fb
prateekb1912 <prateekb1912@gmail.com>
2022-12-15 22:55:08 +0530

    setup flask-socketio

--- server.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import websockets
-
-async def hello():
-    async with websockets.connect("ws://localhost:8080") as websocket:
-        await websocket.send("Hello!")
-        await websocket.recv()
-
-asyncio.run(hello())
