c112ddd7e85bc7b956046634f1c91cdae1cc0e18
prateekb1912 <prateekb1912@gmail.com>
2022-12-15 23:01:08 +0530

    Complete server-side setup

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
+
 socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+    print(f"Message: {msg}")
+    
+    send(msg, broadcast=True)
+
 
 if __name__ == '__main__':
     socketio.run(app)
